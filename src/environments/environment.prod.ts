e576afde43025371ee33785ee533e52dd9726021
Juan perez <garciaianavellaneda@gmail.com>
2022-04-06 12:52:06 -0300

    enviroment.prod para heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyCrEF2mAkH45Kt3c5otkMuj3lmh2JzmXaE",
+    authDomain: "tpsaladejuegos-8b998.firebaseapp.com",
+    projectId: "tpsaladejuegos-8b998",
+    storageBucket: "tpsaladejuegos-8b998.appspot.com",
+    messagingSenderId: "985909907155",
+    appId: "1:985909907155:web:c27c368d47cdb9e7404e26"
+  }
 };
